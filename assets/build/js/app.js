8afd56a15368d790556780c92e95e111c6f3e2ce
oiler <jrf1039@gmail.com>
2018-11-09 12:31:32 -0500

    add js

---
+++ assets/build/js/app.js
@@ -0,0 +1,9 @@
+/*!
+ * project-name v0.0.1: Project description...
+ * (c) 2018 Your Name
+ * MIT License
+ * http://github.com/your-username/project
+ * Open Source Credits: http://comma-separated-list.com, http://of-oss-projects-used.com, http://for-this-project.com
+ */
+
+$(document).foundation()
